b8f8b03518b37701b262c60c2b4f8fb6087c1574
Dan Richelson <dan@launchdarkly.com>
2017-02-02 10:29:34 -0800

    Update tests for Python 2.6

--- testing/test_integration_ldclient.py
+++ testing/test_integration_ldclient.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import pytest
 
@@ -9,26 +10,39 @@ from testing.sync_util import wait_until
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-@pytest.mark.skipif(sdk_key is None, reason="requires LD_SDK_KEY environment variable to be set")
+# skipping for Python 2.6 since it is incompatible with LaunchDarkly's streaming connection due to SNI
+@pytest.mark.skipif(sdk_key is None or sys.version_info < (2, 7),
+                    reason="requires LD_SDK_KEY environment variable to be set")
 def test_ctor_with_sdk_key():
     client = LDClient(sdk_key=sdk_key)
     wait_until(client.is_initialized, timeout=10)
 
     client.close()
 
-
-@pytest.mark.skipif(sdk_key is None, reason="requires LD_SDK_KEY environment variable to be set")
+# skipping for Python 2.6 since it is incompatible with LaunchDarkly's streaming connection due to SNI
+@pytest.mark.skipif(sdk_key is None or sys.version_info < (2, 7),
+                    reason="requires LD_SDK_KEY environment variable to be set")
 def test_ctor_with_sdk_key_and_config():
     client = LDClient(sdk_key=sdk_key, config=Config.default())
     wait_until(client.is_initialized, timeout=10)
 
     client.close()
 
-
-@pytest.mark.skipif(sdk_key is None, reason="requires LD_SDK_KEY environment variable to be set")
+# skipping for Python 2.6 since it is incompatible with LaunchDarkly's streaming connection due to SNI
+@pytest.mark.skipif(sdk_key is None or sys.version_info < (2, 7),
+                    reason="requires LD_SDK_KEY environment variable to be set")
 def test_ctor_with_config():
     client = LDClient(config=Config(sdk_key=sdk_key))
+    wait_until(client.is_initialized, timeout=10)
+
+    client.close()
+
+
+#polling
+@pytest.mark.skipif(sdk_key is None,
+                    reason="requires LD_SDK_KEY environment variable to be set")
+def test_ctor_with_config_polling():
+    client = LDClient(config=Config(sdk_key=sdk_key, stream=False))
     wait_until(client.is_initialized, timeout=10)
 
     client.close()
